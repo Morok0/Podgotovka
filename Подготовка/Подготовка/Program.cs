7d8dfc10b3b58f58253f06d8d3d6ad8f04cbdeda
Morok0 <95218742+Morok0@users.noreply.github.com>
2023-04-19 05:59:37 +0300

    Add files via upload
    Работа со всеми массивами

--- Подготовка/Подготовка/Program.cs
+++ Подготовка/Подготовка/Program.cs
@@ -40,6 +40,88 @@ namespace Подготовка
                 return mas;
             }
         }
+        public class ArreyM
+        {
+            /*
+            internal class Program
+            {
+                static void Main(string[] args)
+                {
+                    int colCount = 6;//m
+                    int rowCount = 5;//n
+                    int[,] arr = GenerateArray(rowCount, colCount);
+                    Console.WriteLine("Исходный массив");
+                    PrintArray(arr);
+                    Console.WriteLine("Сортировка по строкам: ");
+                    int[] row = new int[colCount];
+                    for (int i = 0; i < rowCount; i++)
+                    {
+                        for (int j = 0; j < colCount; j++)
+                            row[j] = arr[i, j];
+                        BubbleSort(row);
+                        Insert(true, i, row, arr);
+                    }
+                    PrintArray(arr);
+                    Console.WriteLine("Сортировка по столбцам: ");
+                    int[] col = new int[rowCount];
+                    for (int i = 0; i < colCount; i++)
+                    {
+                        for (int j = 0; j < rowCount; j++)
+                            col[j] = arr[j, i];
+                        BubbleSort(col);
+                        Insert(false, i, col, arr);
+                    }
+                    PrintArray(arr);
+                }
+                public static void Insert(bool isRow, int dim, int[] source, int[,] dest)
+                {
+                    for (int k = 0; k < source.Length; k++)
+                    {
+                        if (isRow)
+                            dest[dim, k] = source[k];
+                        else
+                            dest[k, dim] = source[k];
+                    }
+                }
+                public static int[,] GenerateArray(int t, int i)
+                {
+                    int[,] table = new int[t, i];
+                    Random random = new Random();
+                    for (int a = 0; a < t; a++)
+                    {
+                        for (int b = 0; b < i; b++)
+                        {
+                            table[a, b] = random.Next(0, 9);
+                        }
+                    }
+                    return table;
+                }
+                public static void PrintArray(int[,] array)
+                {
+                    for (int a = 0; a < array.GetLength(0); a++)
+                    {
+                        for (int b = 0; b < array.GetLength(1); b++)
+                        {
+                            Console.Write(array[a, b] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                static void BubbleSort(int[] inArray)
+                {
+                    for (int i = 0; i < inArray.Length; i++)
+                        for (int j = 0; j < inArray.Length - i - 1; j++)
+                        {
+                            if (inArray[j] > inArray[j + 1])
+                            {
+                                int temp = inArray[j];
+                                inArray[j] = inArray[j + 1];
+                                inArray[j + 1] = temp;
+                            }
+                        }
+                }
+            }*/
+        }
         static void Main(string[] args)
         {   // работа с классом персон
             //Person person = new Person();
@@ -62,6 +144,35 @@ namespace Подготовка
                 Console.WriteLine(mas[i]);
             }
             Console.ReadLine();
+            // Работа с двумерным массивом 
+            Console.Write("Столбцов: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Строк: ");
+            int y = int.Parse(Console.ReadLine());
+            int[,] arr = new int[x, y];
+            Console.WriteLine();
+
+            Console.WriteLine("Заполни матрицу");
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write("mas[" + i + "," + j + "]: ");
+                    arr[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write(" mas[" + i + "," + j + "]: " + arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
