f9443c51a91b5899c6df13b73e49b165931352b2
Morok0 <95218742+Morok0@users.noreply.github.com>
2023-04-19 05:36:12 +0300

    Add files via upload
    Создание методов ввода и вывода

--- Подготовка/Подготовка/Program.cs
+++ Подготовка/Подготовка/Program.cs
@@ -6,17 +6,23 @@ namespace Подготовка
     {
         public class Person
         {
-            public string name = "Undefined";
+            public string name =" ";
             public int age;
 
             public void Print()
             {
                 Console.WriteLine($"Имя: {name}  Возраст: {age}");
             }
+            public void Input()
+            {
+                age = Convert.ToInt32(Console.ReadLine());
+                name = Convert.ToString(Console.ReadLine());
+            }
         }
         static void Main(string[] args)
         {
             Person person = new Person();
+            person.Input();
             person.Print();
         }
     }
