fd4992947ca751cf9d7f7673ea0933c87b5aa4ee
Morok0 <95218742+Morok0@users.noreply.github.com>
2023-04-19 05:49:24 +0300

    Add files via upload
    Работа с одномерным массивом, сортировка пузырьком

--- Подготовка/Подготовка/Program.cs
+++ Подготовка/Подготовка/Program.cs
@@ -19,11 +19,49 @@ namespace Подготовка
                 name = Convert.ToString(Console.ReadLine());
             }
         }
-        static void Main(string[] args)
+        public class Arrey
         {
-            Person person = new Person();
-            person.Input();
-            person.Print();
+            
+            public int[] Sort(int[] mas)
+            {
+                int temp;
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    for (int j = i + 1; j < mas.Length; j++)
+                    {
+                        if (mas[i] > mas[j])
+                        {
+                            temp = mas[i];
+                            mas[i] = mas[j];
+                            mas[j] = temp;
+                        }
+                    }
+                }
+                return mas;
+            }
+        }
+        static void Main(string[] args)
+        {   // работа с классом персон
+            //Person person = new Person();
+            //person.Input();
+            //person.Print();
+            //работа с одномерным массивом 
+            Arrey array = new Arrey();
+            Console.WriteLine("Сколько чисел будем сортировать?");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите числа для сортировки:");
+            int[] mas = new int[N];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            array.Sort(mas);
+            Console.WriteLine("После сортировки:");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine(mas[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
