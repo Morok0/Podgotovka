b2f2d0e5abbbea28072be82a70ddef814cef7b6c
Morok0 <95218742+Morok0@users.noreply.github.com>
2023-04-19 05:24:21 +0300

    Add files via upload
    Создание класса

---
+++ Подготовка/Подготовка/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Подготовка
+{
+    class Program
+    {
+        public class Person
+        {
+            public string name = "Undefined";
+            public int age;
+
+            public void Print()
+            {
+                Console.WriteLine($"Имя: {name}  Возраст: {age}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Print();
+        }
+    }
+}
